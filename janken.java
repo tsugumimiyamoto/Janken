5e01fd49b85be533b595a6d1b81ecb76ed67af41
tsugumimiyamoto <miyamoto@route-zero.com>
2019-02-12 10:31:03 +0900

    Update janken.java
    3kaime

--- janken.java
+++ janken.java
@@ -2,12 +2,17 @@ import java.util.Scanner;
 public class Janken {
    public static void main(String[] args) {
 	   Scanner sc = new Scanner(System.in);
+	   System.out.println("1はグー、2はチョキ、3はパーとする。");
+	   System.out.println("先手入力してください。");
 	   int a = sc.nextInt();
+	   System.out.println("後手入力してください。");
 	   int b = sc.nextInt();
-	   if(a<b) {
-		   System.out.println("グーの勝ちでチョキの負け");
-	   }else {
-	   System.out.println("あいこ");
-	   }
+	   if(a==b) {
+		   System.out.println("あいこ");
+	   }else if(a+2==b || a-1==b || a==1 && b==3 && a==2 && b==1) {
+	   System.out.println("後手の勝ち");
+       }else {
+    	   System.out.println("先手の勝ち");
+       }
    }
 }
